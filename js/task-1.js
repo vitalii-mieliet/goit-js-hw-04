4f82ce370e04bda58719a661068e8ec4adf37b64
Will IM <vitalii.mieliet@gmail.com>
2025-04-15 22:31:22 +0300

    Do 1st task

--- js/task-1.js
+++ js/task-1.js
@@ -9,21 +9,24 @@ containerSize — число, максимальна кількість один
 Функція має повернути результат перевірки, чи помістяться всі товари в контейнер. Тобто порахувати загальну кількість товарів в об’єкті products і повернути true, якщо вона менше або дорівнює containerSize, і false, якщо ні.
  */
 //! ============================
+'use strict';
+
+console.log('Task-1');
+
+function isEnoughCapacity(products, containerSize) {
+  let totalQuantity = 0;
+  for (const quntity of Object.values(products)) {
+    totalQuantity += quntity;
+  }
+  return totalQuantity <= containerSize;
+}
 
 //! ============================
 
-// console.log(
-//   isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
-// ); // true
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
 
-// console.log(
-//   isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
-// ); // false
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
 
-// console.log(
-//   isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
-// ); // true
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
 
-// console.log(
-//   isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
-// ); // false
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
